032779a59d76455dcad0e2adf082a2d4ea782c28
TBXark <tbxark@outlook.com>
2017-05-15 12:33:15 +0800

    Initial Commit

---
+++ HNTaxi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HNTaxi
+//
+//  Created by Tbxark on 15/05/2017.
+//  Copyright © 2017 Tbxark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
